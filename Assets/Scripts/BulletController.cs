1a8fc8d8ccfe283702fbb0472f707c9d66af556c
Konrad <kmarszalek0@gmail.com>
2019-05-28 00:58:40 +0200

    add shooting and killing the enemy

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+	public int damage { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnCollisionEnter(Collision collision)
+    {
+		if(collision.gameObject.name == "Enemy") {
+			collision.gameObject.GetComponent<EnemyController>().Damage(damage, transform.position);
+        }
+		if(collision.gameObject.name != "Player") {
+			Destroy(gameObject);
+		}
+    }
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,43 +9,63 @@ public class EnemyController : MonoBehaviour
 	public int damage;
 	public Transform Player;
 	public float damageRadius;
+	public int knockbackForceMultiplier;
+	public int health;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+		
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(FinalPath != null && FinalPath.Count > 1) 
+		if(health > 0) 
 		{
-			float moveHorizontal = 1;
-			float moveVertical = 1;
-			
-			Node target = FinalPath[1];
-			if(target.vPosition.x - transform.position.x > 1) 
+			if(FinalPath != null && FinalPath.Count > 1) 
 			{
-				moveHorizontal = 1;
+				float moveHorizontal = 1;
+				float moveVertical = 1;
+				
+				Node target = FinalPath[1];
+				if(target.vPosition.x - transform.position.x > 1) 
+				{
+					moveHorizontal = 1;
+				}
+				else if(target.vPosition.x - transform.position.x < -1) 
+				{
+					moveHorizontal = -1;
+				}
+				if(target.vPosition.z - transform.position.z > 1) {
+					moveVertical = 1;
+				}
+				else if(target.vPosition.z - transform.position.z < -1) {
+					moveVertical = -1;
+				}
+				
+				Vector3 movement = new Vector3(moveHorizontal*speed*Time.deltaTime,0.0f,moveVertical*speed*Time.deltaTime);
+				transform.position = transform.position + movement;
 			}
-			else if(target.vPosition.x - transform.position.x < -1) 
+			if(Vector3.Distance(transform.position, Player.transform.position) <= damageRadius)
 			{
-				moveHorizontal = -1;
+				Player.GetComponent<PlayerController>().Damage(damage, transform.position);
 			}
-			if(target.vPosition.z - transform.position.z > 1) {
-				moveVertical = 1;
-			}
-			else if(target.vPosition.z - transform.position.z < -1) {
-				moveVertical = -1;
-			}
-			
-			Vector3 movement = new Vector3(moveHorizontal*speed*Time.deltaTime,0.0f,moveVertical*speed*Time.deltaTime);
-			transform.position = transform.position + movement;
-		}
-		if(Vector3.Distance(transform.position, Player.transform.position) <= damageRadius)
-		{
-			Player.GetComponent<PlayerController>().Damage(damage, transform.position);
 		}
     }
+	
+	public void Damage(int amount, Vector3 takenFrom)
+	{
+		if (health > 0) 
+		{
+			Vector3 knockback = transform.position - takenFrom;
+			knockback.y = 0.25f;
+			knockback = Vector3.Normalize(knockback) * amount * knockbackForceMultiplier;
+			health -= amount;
+			if(health <= 0) {
+				GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+			}
+			GetComponent<Rigidbody>().AddForce(knockback);
+		}
+	}
 }
