c72e0aa15c8e66a8c1bc11998d6f8d55acaa85e3
piwowarc <sumetaz@gmail.com>
2019-05-28 22:01:37 +0200

    kel

---
+++ Assets/Scripts/FinishTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FinishTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().CompleteLevel();
+        }
+
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Image health;
+    public int maxHealth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int playerHealth = FindObjectOfType<PlayerController>().health;
+        health.fillAmount = Mathf.Max(0f, playerHealth) / (float) maxHealth;
+
+    }
+}
