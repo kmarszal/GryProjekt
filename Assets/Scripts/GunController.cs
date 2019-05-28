359ebf49272d9ed90bb7c17bd4e9599d094f820d
Konrad <kmarszalek0@gmail.com>
2019-05-28 22:14:08 +0200

    reload time, ammo and machine gun

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-	public int ammo { get; set; }
+	public int ammo;
 	public int clipSize;
+	protected int ammoInClip;
 	public int reloadTime;
+	protected int reloadTimeNow;
 	public Transform Owner { get; set; }
 	public float bulletVelocity;
 	public int damage;
@@ -14,10 +16,11 @@ public class GunController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+		ammoInClip = clipSize;
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
 		if(Owner != null)
 		{
@@ -28,16 +31,29 @@ public class GunController : MonoBehaviour
 				transform.rotation = rotation;
 			}
 			
-			if (Input.GetMouseButtonDown(0)) 
+			if (Input.GetMouseButtonDown(0) && reloadTimeNow == 0) 
 			{
-				GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				bullet.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
-				bullet.transform.position = transform.position;
-				BulletController bc = bullet.AddComponent<BulletController>() as BulletController;
-				bc.damage = damage;
-				bullet.AddComponent<Rigidbody>();
-				Vector3 bulletDirection = rotation * new Vector3(0, 0, 1);
-				bullet.GetComponent<Rigidbody>().AddForce(bulletDirection * bulletVelocity);
+				if(ammoInClip > 0) {
+					GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					bullet.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+					bullet.transform.position = transform.position;
+					BulletController bc = bullet.AddComponent<BulletController>() as BulletController;
+					bc.damage = damage;
+					bullet.AddComponent<Rigidbody>();
+					Vector3 bulletDirection = rotation * new Vector3(0, 0, 1);
+					bullet.GetComponent<Rigidbody>().AddForce(bulletDirection * bulletVelocity);
+					--ammoInClip;
+				}
+				else if(ammo > 0)
+				{
+					reloadTimeNow = reloadTime;
+					ammo -= clipSize;
+					ammoInClip = ammo > 0 ? clipSize : clipSize + ammo;
+				}
+			}
+			if(reloadTimeNow > 0) 
+			{
+				--reloadTimeNow;
 			}
 		}
     }
